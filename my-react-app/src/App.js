822260c18e42a5849888628c7619af29c9150275
Manojnm24 <manojnm80204@gmail.com>
2025-07-31 17:59:01 +0530

    added json file and few files as well

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,42 +1,35 @@
-// ❌ DO NOT import BrowserRouter here anymore
-// import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
-import { Routes, Route, Link } from 'react-router-dom';
-
+import logo from './logo.svg';
 import './App.css';
-import Login from './Login';
+import Login from './Login'
 import Garage from './Garage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NoPage from './NoPage';
 import Timer from './Timer';
 import Counter from './Counter';
-import TutorialsListComponent from './components/tutorials-list.component';
-import NoPage from './NoPage';
-import UserCrud from './User-Management';
-
+import TutorialsListComponent from './tutorials-list.component';
+import AddTutorial from './AddTutorial';
+import Tutorial from './Tutorial';
+import CounterUsingRedux from './CounterUsingRedux';
+import AppUsingReducer from './AppUsingReducer';
+import UseMemoExample from './UseMemoExample';
+import TutorialsComponent from './TutorialsComponent';
 function App() {
   return (
-    <div>
-      {/* Simple Navigation Bar */}
-      <nav style={{ padding: '10px', backgroundColor: '#f5f5f5' }}>
-        <Link to="/login" style={{ marginRight: '10px' }}>Login</Link>
-        <Link to="/garage" style={{ marginRight: '10px' }}>Garage</Link>
-        <Link to="/timer" style={{ marginRight: '10px' }}>Timer</Link>
-        <Link to="/counter" style={{ marginRight: '10px' }}>Counter</Link>
-        <Link to="/tutorials" style={{ marginRight: '10px' }}>Tutorials</Link>
-        <Link to="/user-crud" style={{ marginRight: '10px' }}>User Management</Link>
-      </nav>
-
-      {/* Routes */}
+    
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route index element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/garage" element={<Garage />} />
-        <Route path="/timer" element={<Timer />} />
-        <Route path="/counter" element={<Counter />} />
-        <Route path="/tutorials" element={<TutorialsListComponent />} />
-        <Route path="/user-crud" element={<UserCrud />} />
+        <Route path="/" element={<AppUsingReducer/>}/>
+        <Route index element={<UseMemoExample />} />
+        <Route path='/login' element={<Login />} />
+        <Route path="/garage" element={<Garage/>}/>
+        <Route path="/timer" element={<Timer/>}/>
+        <Route path="/counter" element={<Counter/>}/>
+         <Route path="/tutorials" element={<TutorialsListComponent/>}/>
+         <Route path="/tutorials/:id" element={<Tutorial/>} />
+         <Route path="/addTutorials" element={<AddTutorial/>}/>
+         <Route path="/tutorialsComponent" element={<TutorialsComponent />} />
         <Route path="*" element={<NoPage />} />
       </Routes>
-    </div>
+    
   );
 }
 
